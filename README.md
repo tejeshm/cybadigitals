984a4a88a8effc9a83820b5135204766522ebbe3
tejesh <tejesh.m.india@gmail.com>
2015-07-04 00:10:31 +0530

    all files

---
+++ README.md
@@ -0,0 +1,3 @@
+# testProject2
+
+a [Sails](http://sailsjs.org) application
---
+++ config/passport.js
@@ -0,0 +1,118 @@
+/**
+ * Passport configuration
+ *
+ * This is the configuration for your Passport.js setup and where you
+ * define the authentication strategies you want your application to employ.
+ *
+ * I have tested the service with all of the providers listed below - if you
+ * come across a provider that for some reason doesn't work, feel free to open
+ * an issue on GitHub.
+ *
+ * Also, authentication scopes can be set through the `scope` property.
+ *
+ * For more information on the available providers, check out:
+ * http://passportjs.org/guide/providers/
+ */
+
+module.exports.passport = {
+  local: {
+    strategy: require('passport-local').Strategy
+  },
+
+  bearer: {
+    strategy: require('passport-http-bearer').Strategy
+  },
+  //
+  //serializeUser: function (user, done) {
+  //  done(null, user.id);
+  //},
+  //
+  //deserializeUser: function (id, done) {
+  //  User.findOne({id: id}, function (err, user) {
+  //    done(err, user);
+  //  });
+  //},
+  //use            : new LocalStrategy({
+  //    usernameField: 'email',
+  //    passwordField: 'password'
+  //  },
+  //  function (email, password, done) {
+  //
+  //    User.findOne({email: email}, function (err, user) {
+  //      if (err) {
+  //        return done(err);
+  //      }
+  //      if (!user) {
+  //        return done(null, false, {message: 'Incorrect email.'});
+  //      }
+  //
+  //      bcrypt.compare(password, user.password, function (err, res) {
+  //        if (!res)
+  //          return done(null, false, {
+  //            message: 'Invalid Password'
+  //          });
+  //        var returnUser = {
+  //          email    : user.email,
+  //          createdAt: user.createdAt,
+  //          id       : user.id
+  //        };
+  //        return done(null, returnUser, {
+  //          message: 'Logged In Successfully'
+  //        });
+  //      });
+  //    });
+  //  }
+  //),
+
+  twitter: {
+    name    : 'Twitter',
+    protocol: 'oauth',
+    strategy: require('passport-twitter').Strategy,
+    options : {
+      consumerKey   : 'your-consumer-key',
+      consumerSecret: 'your-consumer-secret'
+    }
+  },
+
+  github: {
+    name    : 'GitHub',
+    protocol: 'oauth2',
+    strategy: require('passport-github').Strategy,
+    options : {
+      clientID    : 'your-client-id',
+      clientSecret: 'your-client-secret'
+    }
+  },
+
+  facebook: {
+    name    : 'Facebook',
+    protocol: 'oauth2',
+    strategy: require('passport-facebook').Strategy,
+    options : {
+      clientID    : 'your-client-id',
+      clientSecret: 'your-client-secret',
+      scope       : ['email'] /* email is necessary for login behavior */
+    }
+  },
+
+  google: {
+    name    : 'Google',
+    protocol: 'oauth2',
+    strategy: require('passport-google-oauth').OAuth2Strategy,
+    options : {
+      clientID    : 'your-client-id',
+      clientSecret: 'your-client-secret'
+    }
+  },
+
+  cas: {
+    name    : 'CAS',
+    protocol: 'cas',
+    strategy: require('passport-cas').Strategy,
+    options : {
+      ssoBaseURL   : 'http://your-cas-url',
+      serverBaseURL: 'http://localhost:1337',
+      serviceURL   : 'http://localhost:1337/auth/cas/callback'
+    }
+  }
+};
